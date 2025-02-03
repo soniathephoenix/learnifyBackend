b287160f59ce1cf282f4c8ec679ffa0dc9677160
allredmatt <matt@allred.digital>
2025-02-03 12:05:04 +0000

    Add DB connect logic

--- database/connect.js
+++ database/connect.js
@@ -1,1 +1,9 @@
-const pg = require('pg')
+const { Pool } = require("pg");
+
+const db = new Pool({
+    connectionString: process.env.DB_URL
+})
+
+console.log("DB connection established.")
+
+module.exports = db;
