5784cc13eb7dfda5933b02f696f0792c6de5e680
allredmatt <matt@allred.digital>
2025-02-04 14:42:29 +0000

    Add authentication middleware

--- models/User.js
+++ models/User.js
@@ -3,16 +3,17 @@ const Question = require('../models/Question')
 
 class User {
 
-    constructor({ login_id, username, password, name, surname}) {
+    constructor({ login_id, username, password, name, surname, email}) {
         this.login_id = login_id;
         this.username = username;
         this.password = password;
         this.name = name;
         this.surname = surname;
+        this.email = email;
     }
 
     static async getOneById(id) {
-        const response = await db.query("SELECT login_id, username FROM login_info WHERE login_id = $1", [id]);
+        const response = await db.query("SELECT login_id, username, name, surname, email FROM login_info WHERE login_id = $1", [id]);
         if (response.rows.length != 1) {
             throw new Error("Unable to locate user.");
         }
@@ -28,26 +29,29 @@ class User {
     }
 
     static async create(data) {
-        const { name, surname, username, password } = data;
+        const { name, surname, username, password, email } = data;
         if(username == undefined || password == undefined) throw Error("Ensure username and password are both provided")
-        let response = await db.query("INSERT INTO login_info (name, surname, username, password) VALUES ($1, $2, $3, $4) RETURNING login_id;",
-            [name, surname, username, password]);
+        let response = await db.query("INSERT INTO login_info (name, surname, username, password, email) VALUES ($1, $2, $3, $4, $5) RETURNING login_id;",
+            [name, surname, username, password, email]);
         const newId = response.rows[0].login_id;
         const newUser = await User.getOneById(newId);
         return newUser;
     }
 
     async getCurrentQ() {
-        let pointsResponse = await db.query(`
-            SELECT points
-            FROM points_info
-            WHERE user_id = $1;
-        `, [this.login_id])
-        console.log(pointsResponse);
-        const points = pointsResponse.rows[0].points
-        let questionsResponse =  await Question.getByPoints(points + 1)
-        return questionsResponse
-    }
+        try{
+            let pointsResponse = await db.query(`
+                SELECT points
+                FROM points_info
+                WHERE user_id = $1;
+            `, [this.login_id])
+            const points = pointsResponse.rows[0].points
+            let questionsResponse =  await Question.getByPoints(points)
+            return questionsResponse
+        } catch(err){
+            return await Question.getByPoints(0)
+        }
+   }
 }
 
 module.exports = User;
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 
 const userController = require('../controllers/user.js');
+const authenticator = require("../middleware/authenticator");
 
 const userRouter = Router();
 
 userRouter.post("/register", userController.register);
 userRouter.post("/login", userController.login);
-userRouter.get('/currentq/:id', userController.currentQuestion)
+userRouter.get('/currentq/', authenticator, userController.currentQuestion)
 
 module.exports = userRouter;
